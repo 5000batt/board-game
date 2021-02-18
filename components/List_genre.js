6ec22735f150bb296464ea7a22809dbfe7109482
5000batt <5000batt@gmail.com>
2021-02-18 14:05:36 +0900

    List grid

--- components/List_genre.js
+++ components/List_genre.js
@@ -1,41 +1,47 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { FlatGrid } from 'react-native-super-grid';
 
-import { ScrollView } from 'react-native-gesture-handler';
 import { GENREDATA } from '../shared/list_genere';
 
-import { ListItem, Avatar, Card, Icon } from 'react-native-elements';
+import { Card } from 'react-native-elements';
 
-const list = GENREDATA;
-// console.log(list);
+const styles = StyleSheet.create({
+  gridView: {
+    marginTop: 10,
+    flex: 1,
+  },
+  itemContainer: {
+    justifyContent: 'flex-end',
+    borderRadius: 5,
+    padding: 10,
+    height: 170,
+  },
+  itemName: {
+    fontSize: 16,
+    color: '#000000',
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+});
 
 const List = ({ navigation }) => {
   return (
-    <View
-      style={{flex: 1}}>
-        <ScrollView
-          contentContainerStyle={
-            { flexGrow:1, alignItems:"center", justifyContent:"center" }}
-        >
-          {
-            list.map((item, i) => (
-              <Card 
-                containerStyle={{width:"60%"}}
-                key={i}
-              >
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Divider />
-                <Card.Image 
-                  source={{uri: item.image}}
-                  style={{height:300, width:"100%"}}
-                  onPress={()=>{navigation.navigate("List_boardgame", {id: item.id, genre: item.genre})}}
-
-                />          
-              </Card>
-            ))
-          }
-      </ScrollView>
-    </View>
-  )
+    <FlatGrid
+      itemDimension={120}
+      data={GENREDATA}
+      style={styles.gridView}
+      spacing={10}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Card.Image
+            source={{uri: item.image}}
+            onPress={()=>{navigation.navigate("List_boardgame", {id: item.id, genre: item.genre})}}
+          />  
+          <Text style={styles.itemName}>{item.title}</Text>
+        </View>
+      )}
+    />
+  );
 }
 export default List;
